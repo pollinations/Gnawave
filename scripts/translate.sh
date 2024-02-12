d83f0a5da338eb269d5d3ac1248091a4b6073fc4
voodoohop <thomas.haferlach@pixelynx.io>
2024-02-12 17:15:45 +0100

    remove unnecessary files

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#aider one-pager* --message "translate one-pager.md onde-pager-de.md (replace contents if it already exists. you have the ability to translate to german. believe in yourself" --model gpt-4-0125-preview
-aider index* --message "translate index.md index-de.md (replace contents if it already exists. you have the ability to translate to german. believe in yourself" --model gpt-4-0125-preview
+aider one-pager* --message "translate one-pager.md one-pager-de.md (replace the contents if the detination already exists but information is missing.) . you have the ability to translate to german. believe in yourself" --model gpt-4-0125-preview
+aider index* --message "translate index.md index-de.md (replace contents the contents if it already exists. you have the ability to translate to german. believe in yourself" --model gpt-4-0125-preview
